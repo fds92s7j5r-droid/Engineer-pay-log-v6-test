18ade532d4bb405670aa3f152447ca5fb58931ba
fds92s7j5r-droid <fds92s7j5r@privaterelay.appleid.com>
2026-08-17 08:32:17 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='engineer-pay-log-v7-1-0-vacation-special-shift-update-20260817';
+const CACHE_NAME='engineer-pay-log-v7-1-1-silent-unscheduled-ot-eight-hour-floor-20260817';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./icons/presenting-engineer.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
