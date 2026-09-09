2022e1c27e8f055e49fc6bfec85f138de2e911a3
fds92s7j5r-droid <fds92s7j5r@privaterelay.appleid.com>
2026-09-09 19:51:26 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='engineer-pay-log-v10-1-piece5-lifecycle-phonefix-20260909';
+const CACHE_NAME='engineer-pay-log-v10-1-piece5-lifecycle-messagepolish-20260909';
 const APP_SHELL=['./','./index.html','./demo.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./icons/presenting-engineer.jpg'];
 
 self.addEventListener('install',e=>e.waitUntil(
